9cd50a08feeac325f21cf58c5cb8990121a8c01b
saugat110 <105158208+saugat110@users.noreply.github.com>
2024-08-05 19:07:21 +0545

    Initial commit project configuration

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //this method will show home page
+    public function index(){
+        return view ('front.home');
+    }
+
+    
+}
