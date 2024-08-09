396ca56c81f525a2d7f4eac061581e313ebdc543
saugat110 <105158208+saugat110@users.noreply.github.com>
2024-08-09 07:03:19 +0545

    branch11_show_latest_featured_job_committed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     //this method will show home page
     public function index(){
-        return view ('front.home');
+        $categories = Category::where('status', 1) -> orderBy('name', 'asc') -> take(8) -> get();
+
+        $featuredjobs  = Job::where([
+            'status' => 1,
+            'isFeatured' => 1,
+        ])  -> orderby('created_at', 'desc')
+            ->take(6) 
+            -> get();
+            
+        $latestjobs = Job::where('status', 1) -> orderBy('created_at', 'desc') -> take(6) -> get();
+
+
+        return view ('front.home',[
+            'categories' => $categories,
+            'featuredjobs' => $featuredjobs,
+            'latestjobs' => $latestjobs,
+        ]);
     }
 
     
